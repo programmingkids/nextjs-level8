cab1fa1a540f54a6dd468d1186d6f95ea609f557
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-12-23 13:22:53 +0000

    added chapter07 templates

---
+++ chapter07/src/templates/app/work03/page.tsx
@@ -0,0 +1,28 @@
+import { MdPerson } from 'react-icons/md';
+import { MdHome } from 'react-icons/md';
+import { MdEdit } from 'react-icons/md';
+import { MdThumbUp } from 'react-icons/md';
+
+export default function Page() {
+  return (
+    <div className="main">
+      <h1 className="p-4 bg-blue-200 text-xl">Work03</h1>
+      <div className="py-4 px-6">
+        <div className="grid grid-cols-4 gap-5 justify-items-center	mb-4">
+          <MdPerson />
+          <MdHome />
+          <MdEdit />
+          <MdThumbUp />
+          <MdPerson className="text-red-500" />
+          <MdHome className="text-blue-500" />
+          <MdEdit className="text-purple-500" />
+          <MdThumbUp className="text-orange-500" />
+          <MdPerson className="text-red-500 text-lg" />
+          <MdHome className="text-blue-500 text-xl" />
+          <MdEdit className="text-purple-500 text-2xl" />
+          <MdThumbUp className="text-orange-500 text-3xl" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ chapter07/src/templates/components/header.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+const links = [
+  { title: 'Home', href: '/' },
+  { title: 'Work01', href: '/work01' },
+  { title: 'Work02', href: '/work02' },
+  { title: 'Work03', href: '/work03' },
+];
+
+export const Header = function () {
+  return (
+    <div className="main p-4 bg-blue-300">
+      <h1 className="p-2 text-2xl">NextJSアプリ</h1>
+      <div className="flex justify-space flex-wrap p-2">
+        {links.map((e, i) => (
+          <Link key={i} href={e.href} className="text-link mx-3">
+            {e.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
