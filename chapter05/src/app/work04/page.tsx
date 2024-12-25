d78049203603bce952254bf34b94a0fa90adbbc1
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-12-25 12:04:57 +0000

    fix chapter05 comment

--- chapter05/src/app/work04/page.tsx
+++ chapter05/src/app/work04/page.tsx
@@ -3,17 +3,17 @@ export default function Page() {
     <div className="main">
       <h1 className="p-4 bg-blue-200 text-xl">Work04</h1>
       <div className="py-4 px-6">
-        <div className="flex justify-normal	bg-slate-500 p-5 mb-4">
+        <div className="flex justify-normal bg-slate-500 p-5 mb-4">
           <div className="bg-orange-500 text-white p-4">オレンジ</div>
           <div className="bg-purple-500 text-white p-4">グレープ</div>
           <div className="bg-pink-500 text-white p-4">ピーチ</div>
         </div>
-        <div className="flex justify-start	bg-slate-500 p-5 mb-4">
+        <div className="flex justify-start bg-slate-500 p-5 mb-4">
           <div className="bg-orange-500 text-white p-4">オレンジ</div>
           <div className="bg-purple-500 text-white p-4">グレープ</div>
           <div className="bg-pink-500 text-white p-4">ピーチ</div>
         </div>
-        <div className="flex justify-end	bg-slate-500 p-5 mb-4">
+        <div className="flex justify-end bg-slate-500 p-5 mb-4">
           <div className="bg-orange-500 text-white p-4">オレンジ</div>
           <div className="bg-purple-500 text-white p-4">グレープ</div>
           <div className="bg-pink-500 text-white p-4">ピーチ</div>
@@ -23,7 +23,7 @@ export default function Page() {
           <div className="bg-purple-500 text-white p-4">グレープ</div>
           <div className="bg-pink-500 text-white p-4">ピーチ</div>
         </div>
-        <div className="flex justify-between	bg-slate-500 p-5 mb-4">
+        <div className="flex justify-between bg-slate-500 p-5 mb-4">
           <div className="bg-orange-500 text-white p-4">オレンジ</div>
           <div className="bg-purple-500 text-white p-4">グレープ</div>
           <div className="bg-pink-500 text-white p-4">ピーチ</div>
